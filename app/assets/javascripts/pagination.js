a81c09c3ae20426e47329c032f24d2938d9dfb92
Bogdan <b.orlov.fl@yandex.ru>
2015-03-29 21:06:09 +0300

    add pushState

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,14 +1,17 @@
 $(function() {
   $(document).on("click", ".pagination a, th a", function() {
     $(".pagination").html("Page is loading...");
-    $.get(this.href, null, null, "script")
+    $.getScript(this.href);
+    history.pushState(null, document.title, this.href);
     return false;
+  });
+  $("#entities_search input").keyup(function() { //$("#entities_search").submit(function() {
+    $.get($("#entities_search").attr("action"), $("#entities_search").serialize(), null, "script");
+    history.replaceState(null, document.title, $("#entities_search").attr("action") + "?" + $("#entities_search").serialize()); //pushState
+    return false;
+  });
+  $(window).bind("popstate", function() {
+    $.getScript(location.href);
   });
 });
 
-$(function() {
-  $("#entities_search input").keyup(function() { //$("#entities_search").submit(function() {
-    $.get($("#entities_search").attr("action"), $("#entities_search").serialize(), null, "script");
-    return false;
-  });
-});
